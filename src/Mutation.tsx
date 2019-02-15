9ab49ec243f37556d6efe6f0c7b1fe88efc58381
Lukas Kuehnert <lukas.kuehnert@gmail.com>
2019-02-15 17:53:58 +0100

    Add Mutation component

---
+++ src/Mutation.tsx
@@ -0,0 +1,77 @@
+import { ApolloError } from 'apollo-client'
+import React from 'react'
+import {
+  Mutation as ApolloMutation,
+  MutationFn,
+  MutationProps as ApolloMutationProps,
+  MutationResult as ApolloMutationResult,
+  OperationVariables,
+} from 'react-apollo'
+import { Omit } from './types'
+
+interface Loading {
+  type: 'loading'
+}
+
+interface Data<T> {
+  type: 'data'
+  data: T
+}
+
+interface Error {
+  type: 'error'
+  error: ApolloError
+}
+
+export type MutationResult<T> = Loading | Data<T> | Error
+
+export interface MutationProps<TData, TVariables>
+  extends Omit<ApolloMutationProps<TData, TVariables>, 'children'> {
+  children: (
+    mutateFn: MutationFn<TData, TVariables>,
+    result: MutationResult<TData>,
+  ) => React.ReactNode
+}
+
+class Mutation<
+  TData = any,
+  TVariables = OperationVariables
+> extends React.PureComponent<MutationProps<TData, TVariables>> {
+  public render() {
+    return (
+      <ApolloMutation {...this.props}>
+        {(mutateFn, data) => this.props.children(mutateFn, this.mapData(data))}
+      </ApolloMutation>
+    )
+  }
+
+  private mapData = (
+    result: ApolloMutationResult<TData>,
+  ): MutationResult<TData> => {
+    const { loading, error, data } = result
+
+    if (loading) {
+      return { type: 'loading' }
+    }
+    if (error) {
+      return {
+        type: 'error',
+        error,
+      }
+    }
+    if (data) {
+      return {
+        type: 'data',
+        data,
+      }
+    }
+
+    // This point should actually never be reached due to the restrictions of Apollo GraphQL responses
+    console.error(
+      'An unexpected Mutation sidecase happened where no valid union type could be found, please file an issue for this.',
+    )
+    return { type: 'error', error: new ApolloError({}) }
+  }
+}
+
+export default Mutation
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { default as Query } from './Query'
+export { default as Mutation } from './Mutation'
